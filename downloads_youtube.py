aa8fc93302db67274655cd635ce299bd11f3156e
Dadijahnix <111552002+Dadijahnix@users.noreply.github.com>
2022-09-05 22:47:45 -0500

    Update downloads_youtube.py

--- downloads_youtube.py
+++ downloads_youtube.py
@@ -12,7 +12,7 @@ url_video = input("\033[;35m" + "Ingrese la url del video AQUÍ: \n" + "\033[0;m
 
 print("\n")
 
-destino = input("\033[;33m" + "Escriba /sdcard/coloque un nombre donde se almacenara el video: \n \n" + "\033[0;m")
+destino = input("\033[;33m" + "Escriba /sdcard/Luego coloque un nombre, donde se le creará una carpeta para almacenar el video descargado: \n \n" + "\033[0;m")
 
 
 yt = YouTube(url_video)
@@ -23,4 +23,4 @@ video.download(destino)
 print("\n")
 
 print("\033[;32;1m" + nom + " Su Video fu descargado con exito." + "\033[0;m" + destino)
-print("\033[;35m" + "Busquelo en el 'Almacenamiento Interno de su Telefono.'" + "\033[0;m")
+print("\033[;35m" + "Busquelo con el 'Nombre de su carpeta que agrego'." + "\033[0;m")
